3d0435300c5b91985dfd6168ebc9f816b7f4d386
Harvey <harveydangel@gmail.com>
2025-02-19 15:20:25 +0800

    Nav, Home, Footer

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="mt-10 text-center text-sm text-white bg-indigo-900 w-full max-w-screen">
+   <div class="mt-5">
+      <h1 class="text-4xl tracking-wider font-stretch-expanded pt-5">Get in touch</h1>
+      <p class="font-extralight mt-3 px-5">We love to here from you! Whether you have questions, need support, or want to collaborate, feel free to reach out to us.</p>
+   </div>
+   <div class="grid md:grid-cols-3 gap-3 mt-8 px-5">
+      <div class="flex flex-row items-center md:justify-center">
+         <x-heroicon-o-map-pin class="size-7" /><span class="text-sm font-extralight ms-1">Biliran Province State University</span>
+      </div>
+      <div class="flex flex-row items-center md:justify-center">
+         <x-heroicon-o-envelope class="size-7 font-semibold" /><span class="text-sm font-extralight ms-1">bipsuevent@bipsu.edu.ph</span>
+      </div>
+      <div class="flex flex-row items-center md:justify-center">
+         <x-heroicon-o-phone class="size-7 font-semibold" /><span class="text-sm font-extralight ms-1"> 09655632456</span>
+      </div>
+   </div>
+   <div class="mt-10">
+      <h1 class="font-semibold py-5 text-md">© 2025 Event Management System. All rights reserved.<h1>
+   </div>
+</footer>
---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,26 @@
+<section id="home" class="section py-10 h-full max-h-screen">
+   <div class="container mx-auto grid md:grid-cols-2 gap-2 ">
+      <div class="section-header mb-12 items-center">
+         <h2 class="section-title text-6xl font-extrabold text-gray-800 mb-4 mt-10 flex flex-wrap">CREATING EVENTS MADE EASY</h2>
+         <span class="text-gray-900">From pep rallies to parent-teacher meetings, this platform
+            ensures smooth organization and execution. 
+            Create outstanding events with ease and efficiency!</span>
+         <br>
+         <div class="flex flex-row mt-4 gap-4">
+            <x-primary-button>
+               <a href="{{route('register')}}">Get Started</a>
+            </x-primary-button>
+   
+            <x-outlined-primary-button>
+               <a href="{{route('login')}}">Sign in</a>
+            </x-outlined-primary-button>
+         </div>
+      </div>
+      <div>
+         <img src="{{url('images/homePic.png')}}" class="h-full lg:max-h-90 animate-shake" alt="">
+      </div>
+      <div class="row flex flex-wrap -mx-4">
+         
+      </div>
+   </div>
+</section>
