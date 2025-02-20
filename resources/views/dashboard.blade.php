c91c6768b8f89a28aa32928bbfae2acce279f0db
Your Name <calemarkadrian@gmail.com>
2025-02-20 15:04:44 +0800

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="flex justify-center items-center mt-4">
+        <div class="flex space-x-40"> <!-- Increased space between cards -->
+            <div class="bg-purple-400 text-white py-5 px-8 rounded-lg shadow-md ">
+                <span class="text-lg font-bold">2</span>
+                <span>Completed Events</span>
+            </div>
+            <div class="bg-purple-400 text-white py-5 px-8 rounded-lg shadow-md ">
+                <span class="text-xl font-bold">1</span>
+                <span>Pending Events</span>
+            </div>
+            <div class="bg-purple-400 text-white py-5 px-8 rounded-lg shadow-md">
+                <span class="text-lg font-bold">3</span>
+                <span>Declined Events</span>
             </div>
         </div>
     </div>
+
 </x-app-layout>
