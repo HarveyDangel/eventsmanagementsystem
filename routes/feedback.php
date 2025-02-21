c58a56486a7f63e98d4e025defeb879270c8bce1
Harvey <harveydangel@gmail.com>
2025-02-21 10:03:05 +0800

    feedback routes

--- routes/feedback.php
+++ routes/feedback.php
@@ -6,15 +6,16 @@ use App\Http\Controllers\feedbackController;
 // 1 is user 
 // 0 is admin
 
+
+
 Route::middleware(['auth', '1'])->group(function () {
-   Route::get('/feedbacks', [FeedbackController::class, 'index'])->name('feedbacks.index');
    Route::get('/feedbacks/create', [FeedbackController::class, 'create'])->name('feedbacks.create');
    Route::post('/feedbacks', [FeedbackController::class, 'store'])->name('feedbacks.store');
-   Route::get('/feedbacks/{feedback}/edit', [FeedbackController::class, 'edit'])->name('feedbacks.edit');
-   Route::patch('/feedbacks/{feedback}', [FeedbackController::class, 'update'])->name('feedbacks.update');
-   Route::delete('/feedbacks/{feedback}', [FeedbackController::class, 'destroy'])->name('feedbacks.destroy');
 });
 
 Route::middleware(['auth', '0'])->group(function () {
    Route::get('/feedbacks', [FeedbackController::class, 'index'])->name('feedbacks.index');
+   Route::get('/feedbacks/{feedback}/edit', [FeedbackController::class, 'edit'])->name('feedbacks.edit');
+   Route::patch('/feedbacks/{feedback}', [FeedbackController::class, 'update'])->name('feedbacks.update');
+   Route::delete('/feedbacks/{feedback}', [FeedbackController::class, 'destroy'])->name('feedbacks.destroy');
 });
