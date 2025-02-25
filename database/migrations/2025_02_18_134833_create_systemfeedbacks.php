b50730f51e8e82f1f24ae1091307c059cdbf0d66
Harvey <harveydangel@gmail.com>
2025-02-25 10:49:28 +0800

    modify migrations

--- database/migrations/2025_02_18_134833_create_systemfeedbacks.php
+++ database/migrations/2025_02_18_134833_create_systemfeedbacks.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('feedback');
-            $table->enum('status', ['saved', 'posted'])->default('saved');
+            $table->enum('status', ['Saved', 'Posted'])->default('Saved');
             $table->timestamps();
         });
     }
