07e4a240b4eb1e6c08db2c4e1df8d576adba4ff9
Your Name <calemarkadrian@gmail.com>
2025-02-25 09:57:05 +0800

    asdfkglhj

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 
-<nav x-data="{ open: false }" class="bg-indigo-700 border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-purple-700 border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -8,7 +8,7 @@
                 {{-- <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a> 
+                    </a>
                 </div> --}}
 
                 <!-- Navigation Links -->
@@ -27,7 +27,7 @@
                             {{ __('Feedback') }}
                         </x-nav-link>
                     </div>
-                    
+
                 @else
                     <div class="hidden space-x-8 sm:-my-px sm:flex">
                         <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
@@ -40,16 +40,16 @@
                             {{ __('History') }}
                         </x-nav-link>
                         <x-nav-link :href="route('feedbacks.index')" :active="request()->routeIs('feedbacks.index')">
-                            {{ __('Feedback') }} 
+                            {{ __('Feedback') }}
                         </x-nav-link>
-                    </div>          
-                @endif 
+                    </div>
+                @endif
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <div x-data="{ open: false }" class="relative">
-                    <button @click="open = !open" class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                    <button @click="open = !open" class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-purple-700 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                         <div>{{ Auth::user()->name }}</div>
                         <div class="ms-1">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -57,13 +57,13 @@
                             </svg>
                         </div>
                     </button>
-            
+
                     <!-- Dropdown Menu -->
                     <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg z-50">
                         <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
                             {{ __('Profile') }}
                         </a>
-                        
+
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <button type="submit" class="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
@@ -73,7 +73,7 @@
                     </div>
                 </div>
             </div>
-            
+
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
