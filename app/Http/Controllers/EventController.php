fee1257c5ef9e39eb0e1ac2bc445b8872ef256d4
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2025-03-07 14:30:19 +0800

    events section - landing page

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,6 +12,12 @@ class EventController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function welcome()
+    {
+        $events = Event::orderBy('start_date', 'asc')->get(); // Adjust limit as needed
+        return view('welcome', compact('events'));
+    }
+
     public function index()
     {
         //
