e4acae2bc984ff9c396ae745ded08f3cf6f5be0d
Harvey <harveydangel@gmail.com>
2025-07-31 17:31:28 +0800

    responsive home section

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,15 +1,9 @@
-<div id="home">
-
-</div>
-
-<section class="section py-10 h-full max-h-screen">
-    <div class="container mx-auto grid md:grid-cols-2 gap-2 ">
-        <div class="section-header mb-12 items-center">
-            <h2 class="section-title text-6xl font-[1000] text-gray-800 mb-4 mt-10 flex flex-wrap">CREATING EVENTS MADE
-                EASY</h2>
-            <span class="text-gray-900">From pep rallies to parent-teacher meetings, this platform
-                ensures smooth organization and execution.
-                Create outstanding events with ease and efficiency!</span>
+<div id="home"></div>
+<section class="section p-[24px] h-full lg:max-h-screen">
+    <div class="lg:grid lg:grid-cols-2 gap-8">
+        <div class="mb-12 items-center">
+            <h2 class="section-title text-[32px] lg:text-[48px] font-bold text-gray-800 mb-4 flex flex-wrap uppercase">Effortless event planning for every school occassion</h2>
+            <span class="text-gray-900 text-[16px]">Whether it's a pep rally or a parent-teacher conference, plan, manage, and execute events in minutes — all in one place!</span>
             <br>
             <div class="flex flex-row mt-4 gap-4">
                 <x-primary-button>
@@ -21,11 +15,8 @@
                 </x-outlined-primary-button>
             </div>
         </div>
-        <div class="justify-center items-center flex flex-col -mt-4">
+        <div class="justify-center items-center flex flex-col">
             <img src="{{ url('images/homePic.png') }}" class="h-full lg:max-h-90 animate-shake" alt="">
-        </div>
-        <div class="row flex flex-wrap -mx-4">
-
         </div>
     </div>
 </section>
--- resources/views/home/nav.blade.php
+++ resources/views/home/nav.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="border-b border-gray-100 sticky top-0 w-full z-50">
+<nav x-data="{ open: false }" class="border-b bg-white border-gray-100 sticky top-0 w-full z-50">
 
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
